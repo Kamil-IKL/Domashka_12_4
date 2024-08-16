353cf343c68646ef0b80781e3689e1b0ea9b420e
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-08-16 19:13:39 +0500

    Update 12_ikl_M12_DZ_45_unittest.py

--- 12_ikl_M12_DZ_45_unittest.py
+++ 12_ikl_M12_DZ_45_unittest.py
@@ -9,7 +9,7 @@ from Domashka_12_4 import rt_with_exceptions
 
 logging.basicConfig(
     level=logging.INFO,  # уровень логирования
-    filemode='w',  # Название файла # Режим - 'w' для перезаписи
+    filemode='w',  # Название файла # Режим - 'w' для перезаписи, "а" - для добавления
     filename='runner_tests.log',  # Название файла
     encoding='utf-8',  # Кодировка
     format='%(asctime)s | %(levelname)s | %(message)s')  # Формат вывода
@@ -19,27 +19,27 @@ class RunnerTest(unittest.TestCase):
     def test_walk(self):
         try:
             name_r1 = 'ВАСЯ'
-            r1 = rt_with_exceptions.Runner(name_r1, 10)
+            r1 = rt_with_exceptions.Runner(name_r1, -10)
             for i in range(10):
                 r1.walk()
             self.assertEqual(r1.distance, 50)
             logging.info('"test_walk" выполнен успешно')
         except:
-            logging.warning(f'"test_walk", неверная скорость для Runner(бегуна): {name_r1}')
+            logging.warning(f'"test_walk", неверная скорость для Runner(бегуна): {name_r1}', exc_info=True)
 
     def test_run(self):
         try:
-            name_r2 = '123456'
+            name_r2 = 123456
             r2 = rt_with_exceptions.Runner(name_r2, 5)
             for i in range(10):
                 r2.run()
             logging.info('"test_run" выполнен успешно')
         except:
-            logging.warning(f'"test_run", неверный тип данных для объекта Runner(бегуна): {name_r2}')
+            logging.warning(f'"test_run", неверный тип данных для объекта Runner(бегуна): {name_r2}', exc_info=True)
 
     def test_challenge(self):
         try:
-            r1 = rt_with_exceptions.Runner('Валера', -25)
+            r1 = rt_with_exceptions.Runner('Валера', 25)
             r2 = rt_with_exceptions.Runner('Явдат', 320)
             for i in range(10):
                 r1.walk()
@@ -47,7 +47,7 @@ class RunnerTest(unittest.TestCase):
             self.assertNotEqual(r1.distance, r2.distance)
             logging.info('"test_challenge" успешно пройден')
         except:
-            logging.warning(f'"test_challenge", неверно указан аргумент для Runner(бегуна)')
+            logging.warning(f'"test_challenge", неверно указан аргумент для Runner(бегуна)', exc_info=True)
 
 
 """
